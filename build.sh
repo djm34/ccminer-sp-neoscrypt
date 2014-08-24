7f6c16ca81dc384401789abf0ecd0ec99952626a
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2014-08-24 19:07:01 +0200

    update configure for mpir lib

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Simple script to create the Makefile
-# then type 'make'
+# Simple script to create the Makefile and build
 
 # export PATH="$PATH:/usr/local/cuda/bin/"
 
-#make clean || echo clean
+make clean || echo clean
 
 rm -f Makefile.in
 rm -f config.status
 ./autogen.sh || echo done
 
-CFLAGS="-O2" ./configure
+CFLAGS="-O2" ./configure --with-mpir-src=../mpir-2.6.0
+
+make
